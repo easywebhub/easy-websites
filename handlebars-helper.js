7424c35c64928322dccc31e3b971ca77cb74a70f
ndqphuong <nhudinhquocphuong@gmail.com>
2016-08-16 10:52:30 +0700

    category changed to AllCategory

--- handlebars-helper.js
+++ handlebars-helper.js
@@ -6,12 +6,7 @@ var moment = require('moment');
 
 module.exports = function (Handlebars) {
     Handlebars.registerHelper(HandlebarsLayouts(Handlebars));
-  //  Helpers({handlebars: Handlebars});
-    ['array', 'code', 'collection', 'comparison', 'date', 'fs', 'html', 'i18n', 'inflection', 'logging', 'markdown', 'match', 'math', 'misc', 'number', 'path', 'string', 'url'].forEach(function(name) {
-        Helpers[name]({
-        handlebars: Handlebars
-        });
-    });
+    Helpers({handlebars: Handlebars});
 
     // dang ky rivetData helper block cho handlebars ở đây
 
@@ -27,12 +22,33 @@ module.exports = function (Handlebars) {
         return JSON.stringify(obj);
     });
 
-	Handlebars.registerHelper('toString', function (obj) {
-        return obj.toString();
-    });
-    
     Handlebars.registerHelper('removeIndex', function (url) {
         return url.replace('index.html', '');
+    });
+    
+    // child path must be a fulll path e.g. 'tin-tuc.tin-van-hoa', 'tin-tuc.tin-the-thao.quoc-te'
+    /*
+    var data = {
+		'tin-tuc': ['1', '2', '3'],
+		'tin-tuc.tin-van-hoa': ['4', '4', '4'],
+		'tin-tuc.tin-the-thao': ['5', '5', '5'],
+		'tin-tuc.tin-the-thao.quoc-te': ['7', '7', '7'],
+		'tin-tuc.tin-kinh-te': ['6', '6', '6'],
+	}
+	(lookupChild data  'tin-tuc')
+	result: ["tin-van-hoa", "tin-the-thao", "tin-kinh-te"]
+    */
+    Handlebars.registerHelper('lookupChild', function (obj, childPath) {
+		var ret = {};
+		for (var key in obj) {
+			if (!obj.hasOwnProperty(key)) continue;
+			if (key.startsWith(childPath)) {
+				var chunks = key.substr(childPath.length).split('.');
+				if (chunks.length > 1)
+					ret[chunks[1]] = true;
+			}
+		}
+		return Object.keys(ret);
     });
 
     Handlebars.registerHelper('formatDate', function (context, options) {
@@ -45,3 +61,4 @@ module.exports = function (Handlebars) {
         return moment(context).format(format);
     });
 };
+
